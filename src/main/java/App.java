a11eba91a011b96ab971a43c8babd1b7caf783ab
joong2043 <tbvjqlqlqlr@gmail.com>
2022-07-16 15:38:13 +0900

    App에 있는 코드 중 method들을 Controller로 옮겨줌 + Controller에 스캐너를 받는 생성자 만들어줘서 App과 연결되게 해줌

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,40 +4,39 @@ import java.util.stream.Stream;
 
 public class App {
     Scanner sc;
-    int lastId;
 
-    ArrayList<WiseSaying> wiseSayings = new ArrayList<>();
-
+    String command;
     public App(){
-        sc = new Scanner(System.in);
-        lastId = 0;
+        this.sc = new Scanner(System.in);
     }
+
 
     void run(){
         System.out.println("== 명언 SSG ==");
 
+        WiseSayingController wiseSayingController = new WiseSayingController(sc);
 
         outer:
         while (true){
             System.out.printf("명령) ");
-            String cmd = sc.nextLine();
-            Rq rq = new Rq(cmd);
+            String cmd = sc.nextLine().trim();
+            Rq rq= new Rq(cmd);
 
             switch (rq.getPath()){
                 case "등록":
-                    write();
+                    wiseSayingController.write();
                     break;
 
                 case "목록":
-                    list();
+                    wiseSayingController.list();
                     break;
 
                 case "삭제":
-                    remove(rq);
+                    wiseSayingController.remove(rq);
                     break;
 
                 case "수정":
-                    modify(rq);
+                    wiseSayingController.modify(rq);
                     break;
 
                 case "종료":
@@ -47,61 +46,6 @@ public class App {
         }
     }
 
-    void write(){
-        lastId++;
-        System.out.printf("명언 : ");
-        String content = sc.nextLine();
-        System.out.printf("작가 : ");
-        String author = sc.nextLine();
 
-        WiseSaying addWiseSaying = new WiseSaying(lastId,content,author);
-        wiseSayings.add(addWiseSaying);
-        System.out.println(addWiseSaying);
-    }
-    void list(){
-        System.out.println("===명언 목록===");
-        System.out.println("번호 / 명언 / 작가");
-        Stream<WiseSaying> wiseSayingStream = wiseSayings.stream();
-        wiseSayingStream.forEach(element -> System.out.print(element));
-    }
-
-    private void remove(Rq rq){
-
-        int id = rq.getIntParam("id",0);
-
-        if(id==0){
-            System.out.println("삭제할 id를 입력해주세요.");
-            return;
-        }
-
-        wiseSayings.remove((id-1));
-
-        lastId--;
-        System.out.printf("%d번 명언을 삭제합니다. \n",(id));
-
-
-    }
-
-    void modify(Rq rq) {
-        int id = rq.getIntParam("id",0);
-
-        if(id==0){
-            System.out.println("수정할 id를 입력해주세요.");
-            return;
-        }
-
-        WiseSaying modifyWiseSaying = wiseSayings.get(id-1);
-
-        System.out.printf("명언(기존) : %s\n",modifyWiseSaying.content);
-        System.out.printf("명언 : ");
-        String content = sc.nextLine();
-        modifyWiseSaying.content = content;
-
-        System.out.printf("작가(기존) : %s\n",modifyWiseSaying.author);
-        System.out.printf("작가 : ");
-        String author = sc.nextLine();
-        modifyWiseSaying.author=author;
-
-    }
 
 }
--- src/main/java/WiseSayingController.java
+++ src/main/java/WiseSayingController.java
@@ -1,2 +1,76 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class WiseSayingController {
+    // 등록, 목록, 삭제, 수정 기능 여기에 일단 다 넣기
+
+    int lastId;
+    List<WiseSaying> wiseSayings;
+
+    Scanner sc;
+    public WiseSayingController(Scanner sc){
+        lastId=0;
+        wiseSayings = new ArrayList<WiseSaying>();
+        this.sc = sc;
+    }
+
+    void write(){
+        lastId++;
+        System.out.printf("명언 : ");
+        String content = sc.nextLine();
+        System.out.printf("작가 : ");
+        String author = sc.nextLine();
+
+        WiseSaying addWiseSaying = new WiseSaying(lastId,content,author);
+        wiseSayings.add(addWiseSaying);
+        System.out.println(addWiseSaying);
+    }
+    void list(){
+        System.out.println("===명언 목록===");
+        System.out.println("번호 / 명언 / 작가");
+        Stream<WiseSaying> wiseSayingStream = wiseSayings.stream();
+        wiseSayingStream.forEach(element -> System.out.print(element));
+    }
+
+    public void remove(Rq rq){
+
+        int id = rq.getIntParam("id",0);
+
+        if(id==0){
+            System.out.println("삭제할 id를 입력해주세요.");
+            return;
+        }
+
+        wiseSayings.remove((id-1));
+
+        lastId--;
+        System.out.printf("%d번 명언을 삭제합니다. \n",(id));
+
+
+    }
+
+    void modify(Rq rq) {
+        int id = rq.getIntParam("id",0);
+
+        if(id==0){
+            System.out.println("수정할 id를 입력해주세요.");
+            return;
+        }
+
+        WiseSaying modifyWiseSaying = wiseSayings.get(id-1);
+
+        System.out.printf("명언(기존) : %s\n",modifyWiseSaying.content);
+        System.out.printf("명언 : ");
+        String content = sc.nextLine();
+        modifyWiseSaying.content = content;
+
+        System.out.printf("작가(기존) : %s\n",modifyWiseSaying.author);
+        System.out.printf("작가 : ");
+        String author = sc.nextLine();
+        modifyWiseSaying.author=author;
+
+    }
+
 }
