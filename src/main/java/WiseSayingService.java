0190125097f74ee4abbbbcdc2f80cce28a531843
joong2043 <tbvjqlqlqlr@gmail.com>
2022-07-16 15:45:01 +0900

    WiseSayingService에 필요할 것 같은 메서드들 작성

--- src/main/java/WiseSayingService.java
+++ src/main/java/WiseSayingService.java
@@ -1,2 +1,27 @@
+import java.util.List;
+
 public class WiseSayingService {
+    List<WiseSaying> wiseSayings;
+
+
+
+    public WiseSayingService(){
+
+    }
+
+    void addWiseSaying(){
+
+    }
+
+    void listWiseSaying(){
+
+    }
+
+    void removeWiseSaying(){
+
+    }
+
+    void modifyWiseSaying(){
+
+    }
 }
