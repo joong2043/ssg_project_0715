37b3c1ac2e3907dd2b415630105e1fb1f29548fb
joong2043 <tbvjqlqlqlr@gmail.com>
2022-07-16 17:24:44 +0900

    Service 클래스 내에 데이터와 직접적으로 닿아있는 코드들을 모두 Repository 클래스에 옮겨주고 데이터 객체를 생성해줘서 기본적인 기능들을 하도록 코드를 만들어주었다

--- src/main/java/WiseSayingRepository.java
+++ src/main/java/WiseSayingRepository.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class WiseSayingRepository {
+    List<WiseSaying> wiseSayings;
+
+    public WiseSayingRepository(){
+        wiseSayings = new ArrayList<WiseSaying>();
+    }
+
+    public void add(WiseSaying wiseSaying){
+        wiseSayings.add(wiseSaying);
+    }
+
+    public void list(){
+        Stream<WiseSaying> wiseSayingStream = wiseSayings.stream();
+        wiseSayingStream.forEach(element -> System.out.print(element));
+    }
+
+    public void remove(int id){
+        wiseSayings.remove(id-1);
+    }
+
+    public void modify(int id, String content, String author){
+        WiseSaying modifyWiseSaying = wiseSayings.get(id-1);
+        modifyWiseSaying.content=content;
+        modifyWiseSaying.author=author;
+    }
+
+    WiseSaying find(int id){
+        WiseSaying foundWiseSaying = wiseSayings.get(id-1);
+
+        return foundWiseSaying;
+    }
+
 }
--- src/main/java/WiseSayingService.java
+++ src/main/java/WiseSayingService.java
@@ -3,34 +3,33 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class WiseSayingService {
-    List<WiseSaying> wiseSayings;
 
     public WiseSayingService(){
-        wiseSayings = new ArrayList<WiseSaying>();
+
     }
+    WiseSayingRepository wiseSayingRepository = new WiseSayingRepository();
+
+
+
 
     void addFunction(WiseSaying wiseSaying){
-        wiseSayings.add(wiseSaying);
+        wiseSayingRepository.add(wiseSaying);
     }
 
     void listFunction(){
-        Stream<WiseSaying> wiseSayingStream = wiseSayings.stream();
-        wiseSayingStream.forEach(element-> System.out.print(element));
+        wiseSayingRepository.list();
     }
 
     void removeFunction(int id){
-        wiseSayings.remove(id-1);
+        wiseSayingRepository.remove(id);
     }
 
     public void modifyFunction(int id, String content, String author){
-        WiseSaying modifyWiseSaying = wiseSayings.get(id-1);
-        modifyWiseSaying.content=content;
-        modifyWiseSaying.author=author;
+        wiseSayingRepository.modify(id,content,author);
     }
 
     WiseSaying find(int id){
-        WiseSaying foundWiseSaying = wiseSayings.get(id-1);
-
-        return foundWiseSaying;
+        WiseSaying wiseSaying = wiseSayingRepository.find(id);
+        return wiseSaying;
     }
 }
